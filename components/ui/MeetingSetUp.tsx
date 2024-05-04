8b76b76256abf26ae7b88bd40222c18a2c3f1f12
raphael <you@example.com>
2024-05-04 19:56:25 +0100

    fix: Add check to ensure the used enabled camera access

--- components/ui/MeetingSetUp.tsx
+++ components/ui/MeetingSetUp.tsx
@@ -6,6 +6,7 @@ import {
 } from "@stream-io/video-react-sdk";
 import React, { useEffect, useState } from "react";
 import { Button } from "./button";
+import { toast } from "sonner";
 
 const MeetingSetUp = ({
   setIsSetUpComplete,
@@ -17,6 +18,29 @@ const MeetingSetUp = ({
   if (!call) {
     throw new Error("useCall must be used within SteamCall component");
   }
+
+  useEffect(() => {
+    let isMounted = true;
+
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(() => {
+        console.log("Access granted");
+      })
+      .catch(() => {
+        // Handle access denied
+        if (isMounted) {
+          toast.error("Please allow camera access in site settings");
+        }
+
+        console.log("Access denied");
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   useEffect(() => {
     if (isMicCamToggledOn) {
       call?.camera.disable();
@@ -30,7 +54,9 @@ const MeetingSetUp = ({
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-2xl font-bold">Setup</h1>
+
       <VideoPreview />
+
       <div className="flex h-16 items-center justify-center gap-3">
         <label className="flex items-center justify-center gap-2 font-medium">
           <input
